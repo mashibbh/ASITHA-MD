be3105373a1e384cc19f1797659d8ef55132e8da
mashibbh <mashidilhan25@gmail.com>
2024-11-02 08:01:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,38 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2zQygRBZ#Vt2dppWtMU4WKWeeiVbrDr4I6GEAT15PDX2RhrmR2zs",
+MONGODB: process.env.MONGODB || "name: Node.js CI
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
